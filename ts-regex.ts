68def4bb74328424243ce499101523a7814a70d7
Lukas Bach <lbach@outlook.de>
2022-06-29 01:53:18 +0200

    new matcher logic

--- ts-regex.ts
+++ ts-regex.ts
@@ -2,9 +2,27 @@
 
 import { CharTable, InvertedCharTable } from './char-table';
 
-// utils
 
-// counter logic
+                                                                      //////////////////////////////////////////////////
+                                                                          ///////////////////////////////// UTILITIES //
+                                                                              //////////////////////////////////////////
+
+type And<A extends boolean, B extends boolean, C extends boolean = true, D extends boolean = true, E extends boolean = true, F extends boolean = true> =
+  A extends true ? B extends true ? C extends true ? D extends true ? E extends true ? F extends true ? true : false : false : false : false : false : false;
+type Or<A extends boolean, B extends boolean, C extends boolean = false, D extends boolean = false, E extends boolean = false, F extends boolean = false> =
+  A extends true ? true : B extends true ? true : C extends true ? true : D extends true ? true : E extends true ? true : F extends true ? true : false;
+type Not<T extends boolean> = T extends true ? false : true;
+type IsEmpty<T extends string> = T extends "" ? true : false;
+type IsNotEmpty<T extends string> = Not<IsEmpty<T>>;
+type IsEqual<A extends string, B extends string> = And<A extends B ? true : false, B extends A ? true : false>;
+type IsInequal<A extends string, B extends string> = Not<IsEqual<A, B>>;
+type Extends<A, B> = A extends B ? true : false;
+
+                                                                      //////////////////////////////////////////////////
+                                                                          ///////////////////////////// COUNTER LOGIC //
+                                                                              //////////////////////////////////////////
+
+// ----------------------------------------------------------------------------------------------------- Decrease --- //
 type DecreaseDigitMap = {
     "9": "8",
     "8": "7",
@@ -30,6 +48,8 @@ type Decrease<T extends string> =
   ? `${A}${Decrease<B>}`
   : "0";
 
+
+// ----------------------------------------------------------------------------------------------------- Increase --- //
 type IncreaseDigitMap = {
     "0": "1",
     "1": "2",
@@ -44,16 +64,58 @@ type IncreaseDigitMap = {
 
 type Increase<T extends string> =
   T extends ""
-  ? ""
-  : T extends keyof IncreaseDigitMap
-  ? IncreaseDigitMap[T]
-  : T extends `${infer A}9`
-  ? (Increase<A> extends "" ? "10" : `${Increase<A>}0`)
-  : T extends `${infer A}${infer B}`
-  ? `${A}${Increase<B>}`
-  : "0";
+    ? ""
+    : T extends keyof IncreaseDigitMap
+      ? IncreaseDigitMap[T]
+      : T extends `${infer A}9`
+        ? (Increase<A> extends "" ? "10" : `${Increase<A>}0`)
+        : T extends `${infer A}${infer B}`
+          ? `${A}${Increase<B>}`
+          : "0";
 
-// range logic
+
+// --------------------------------------------------------------------------------------------------- Comparison --- //
+type IsLonger<a extends string, b extends string> =
+  a extends `${infer a1}${infer a2}`
+    ? b extends `${infer b1}${infer b2}`
+      ? And<IsNotEmpty<a2>, IsNotEmpty<b2>> extends true
+          ? IsLonger<a2, b2>
+      : IsNotEmpty<a2>
+    : never
+  : never;
+type IsDigitLarger<a extends string, b extends string, counter extends string = "0"> =
+  counter extends "10"
+    ? false
+    : a extends b
+      ? false
+      : a extends counter
+        ? false
+        : b extends counter
+          ? true
+          : IsDigitLarger<a, b, Increase<counter>>;
+type IsLarger<a extends string, b extends string> =
+  IsEqual<a, b> extends true
+    ? false
+    : IsLonger<a, b> extends true
+      ? true
+      : IsLonger<b, a> extends true
+        ? false
+        : a extends `${infer a1}${infer a2}`
+          ? b extends `${infer b1}${infer b2}`
+            ? IsEqual<a1, b1> extends true
+              ? IsLarger<a2, b2>
+              : IsDigitLarger<a1, b1>
+            : never
+          : never;
+type IsSmaller<a extends string, b extends string> = And<Not<IsLarger<a, b>>, Not<IsEqual<a, b>>>;
+type IsLargerEquals<a extends string, b extends string> = Or<IsLarger<a, b>, IsEqual<a, b>>;
+type IsSmallerEquals<a extends string, b extends string> = Or<IsSmaller<a, b>, IsEqual<a, b>>;
+
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          /////////////////////////////// RANGE LOGIC //
+                                                                              //////////////////////////////////////////
 type CharTableRange<from extends string, to extends string> =
   from extends to
   ? (from extends keyof CharTable ? CharTable[from] : never)
@@ -76,7 +138,14 @@ type CharRange<range extends string> =
       ? char | CharRange<rest>
       : never;
 
-// basic symbols
+type RepeatString<text extends string, count extends string> =
+  count extends "0" ? "" : `${text}${RepeatString<text, Decrease<count>>}`;
+
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          ///////////////////////////// BASIC SYMBOLS //
+                                                                              //////////////////////////////////////////
 type whitespace = "\n" | " " | "    ";
 type digit = CharRange<"0-9">;
 type word = CharRange<"a-zA-Z0-9">;
@@ -91,43 +160,110 @@ type numberString = `${number}`;
 type IsNumberString<str extends string> = str extends numberString ? true : false;
 
 
-type Quantified<T extends string, count extends string> = count extends "0" ? "" : `${T}${Quantified<T, Decrease<count>>}`;
 
-type And<A extends boolean, B extends boolean, C extends boolean = true, D extends boolean = true, E extends boolean = true, F extends boolean = true> =
-    A extends true ? B extends true ? C extends true ? D extends true ? E extends true ? F extends true ? true : false : false : false : false : false : false;
-type Or<A extends boolean, B extends boolean, C extends boolean = false, D extends boolean = false, E extends boolean = false, F extends boolean = false> =
-    A extends true ? true : B extends true ? true : C extends true ? true : D extends true ? true : E extends true ? true : F extends true ? true : false;
-
-
-// regex components
-// TODO is-token-checks required?
+                                                                      //////////////////////////////////////////////////
+                                                                          ////////////////////////// REGEX COMPONENTS //
+                                                                              //////////////////////////////////////////
+// ------------------------------------------------------------------------------------------------------- Groups --- //
 type IsGroup<regex extends string> = regex extends `(${"?:" | ""}${string})${string}` ? true : false;
 type IsGroupToken<regex extends string> = regex extends `(${"?:" | ""}${string})` ? true : false;
 type Group<regex extends string> = regex extends `(${"?:" | ""}${infer groupContent})${infer rest}` ? `${Regex<groupContent>}${Regex<rest>}` : never;
 
+
+// -------------------------------------------------------------------------------------------- Character classes --- //
 type IsWordSymbol<regex extends string> = regex extends `\w${string}` ? true : false;
 type IsWordSymbolToken<regex extends string> = regex extends `\w` ? true : false;
 type WordSymbol<regex extends string> = regex extends `\w${infer rest}` ? `${word}${Regex<rest>}` : never;
 
+// TODO is-token-checks required?
 type IsToken<regex extends string> = Or<IsWordSymbolToken<regex>, IsGroupToken<regex>, IsAnyChar<regex>, IsCharRangeGroupToken<regex>>;
 
+
+// -------------------------------------------------------------------------------------------------- Quantifiers --- //
 type IsQuantifier<regex extends string> =
     regex extends `${infer token}{${infer quantity}}${string}`
     ? And<IsToken<token>, IsNumberString<quantity>> extends true
     ? true : false : false;
 type Quantifier<regex extends string> =
     regex extends `${infer token}{${infer quantity}}${infer rest}`
-    ? `${Quantified<Regex<token>, quantity>}${Regex<rest>}`
+    ? `${RepeatString<Regex<token>, quantity>}${Regex<rest>}`
     : never;
+type MatchQuantifier<regex extends string, test extends string> =
+  regex extends `${infer token}{${infer quantity}}${infer regexRest}`
+    ? test extends `${RepeatString<Regex<token>, quantity>}${infer testRest}`
+      ? Match<regexRest, testRest>
+      : false
+    : false;
 
+// returns remaining string or never if not matched
+type ProcessQuantifier<regexPart extends string, test extends string, min extends string, max extends string, count extends string = "0"> =
+  IsLargerEquals<count, max> extends true
+    ? test
+    : test extends `${Regex<regexPart>}${infer testRest}`
+      ? ProcessQuantifier<regexPart, testRest, min, max, Increase<count>>
+      : IsSmaller<count, min> extends true
+        ? never
+        : test;
+type ParseQuantityMin<quantity extends string> =
+  quantity extends numberString
+    ? quantity
+    : quantity extends `${infer min},${string}`
+      ? min
+      : "0";
+type ParseQuantityMax<quantity extends string> =
+  quantity extends numberString
+    ? quantity
+    : quantity extends `${string},${infer max}`
+      ? max
+      : "100";
+type MatchQuantifier2<regex extends string, test extends string> =
+  regex extends `${infer token}{${infer quantity}}${infer regexRest}`
+    ? ProcessQuantifier<token, test, ParseQuantityMin<quantity>, ParseQuantityMax<quantity>> extends false
+      ? false
+      : Match<regexRest, ProcessQuantifier<token, test, ParseQuantityMin<quantity>, ParseQuantityMax<quantity>>>
+    : false;
+
+typeAssert<Test<ParseQuantityMin<"3,4">, "3">>();
+typeAssert<Test<ParseQuantityMax<"3,4">, "4">>();
+typeAssert<Test<ProcessQuantifier<"a", "aaaaaaax", "1", "6">, "ax">>();
+typeAssert<Test<ProcessQuantifier<"a", "aaxxxxxx", "1", "6">, "xxxxxx">>();
+typeAssert<Test<ProcessQuantifier<"a", "axxxxxxx", "2", "6">, never>>();
+assertMatch<MatchQuantifier2<"[a-zA-Z]{4,5}", "aaDSa">>();
+assertNoMatch<MatchQuantifier2<"[a-zA-Z]{4,5}", "aaDSDa">>();
+
+
+// ------------------------------------------------------------------------------------------------- Range groups --- //
 type IsCharRangeGroup<regex extends string> = regex extends `[${string}]${string}` ? true : false;
 type IsCharRangeGroupToken<regex extends string> = regex extends `[${string}]` ? true : false;
 type CharRangeGroup<regex extends string> =
   regex extends `[${infer range}]${infer rest}`
     ? `${CharRange<range>}${Regex<rest>}`
     : never;
+type MatchCharRangeGroup<regex extends string, test extends string> =
+  regex extends `[${infer range}]${infer regexRest}`
+    ? test extends `${CharRange<range>}${infer testRest}`
+      ? Match<regexRest, testRest>
+      : false
+    : false;
 
-// main type
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          ////////////////////////////// MATCHER TYPE //
+                                                                              //////////////////////////////////////////
+type Match<regex extends string, test extends string> =
+  And<regex extends "" ? true : false, test extends "" ? true : false> extends true ? true
+  : test extends never ? false
+  : IsQuantifier<regex> extends true ? MatchQuantifier2<regex, test>
+  : IsCharRangeGroup<regex> extends true ? MatchCharRangeGroup<regex, test>
+  : false;
+
+
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          //////////////////////////////// REGEX TYPE //
+                                                                              //////////////////////////////////////////
 type Regex<S extends string> =
     // S extends `${infer A}{${infer B}}${infer C}`
     // ? `${Quantified<Regex<A>, B>}${Regex<C>}`
@@ -159,11 +295,20 @@ type Regex<S extends string> =
     ? `${anyChar}${Regex<A>}`
     : S;
 
-// experiments
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          /////////////////////////////// Experiments //
+                                                                              //////////////////////////////////////////
 type X = "[a]" extends `[${infer A}]${infer B}` ? [A, B] : never;
 type Y = "abcdef" extends `${infer A}${infer B}` ? [A, B] : never;
+type Z = "a" extends `${infer A}${infer B}` ? [A, B] : never;
 
-// testing library
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          /////////////////////////// TESTING LIBRARY //
+                                                                              //////////////////////////////////////////
 type Pass = 'pass';
 type Test<T, U> = [U] extends [T]
     ? Pass
@@ -175,8 +320,14 @@ type TestBothWays<T, U> = [T] extends [U]
     : { actual: T; expected: U };
 
 function typeAssert<T extends Pass>() {}
+function assertMatch<T extends true>() {}
+function assertNoMatch<T extends false>() {}
 
-// unit tests
+
+
+                                                                      //////////////////////////////////////////////////
+                                                                          //////////////////////////////// UNIT TESTS //
+                                                                              //////////////////////////////////////////
 typeAssert<TestBothWays<Regex<"">, "">>();
 typeAssert<TestBothWays<Regex<"[abc]">, "a" | "b" | "c">>();
 typeAssert<TestBothWays<Regex<"\w\d\s">, `${word}${digit}${whitespace}`>>();
@@ -185,3 +336,11 @@ typeAssert<Test<Regex<"[abc]|\dxx">, "a" | "b" | "c" | `${digit}xx`>>();
 typeAssert<Test<Regex<"(ab){4}">, "abababab">>();
 typeAssert<Test<Regex<"a{10}">, "aaaaaaaaaa">>();
 typeAssert<Test<Regex<"[a-zA-Z]{2}\d">, "aD4">>();
+
+assertMatch<Match<"", "">>();
+assertMatch<Match<"[abc]", "a">>();
+assertMatch<Match<"[abc][def]", "ad">>();
+assertMatch<Match<"[a-z][A-Z0-9][0-9]", "aD4">>();
+assertMatch<Match<"[a-z]{20}", "aaaaaaaaaaaaaaaaaaaa">>();
+
+assertNoMatch<Match<"[abc][def]", "ag">>();
